f35b5e7c12d7bce35f39df2cfaa416098006b59c
Maksim Manylov <manylov@gmail.com>
2024-02-09 00:11:16 +0600

    sha1200

--- phase1/multi-proof-verification/input-gen.ts
+++ phase1/multi-proof-verification/input-gen.ts
@@ -61,5 +61,5 @@ const writeToJSONFile = (n: number) => {
 };
 
 // Example usage
-const N = 700; // Replace with the desired number
+const N = 1200; // Replace with the desired number
 writeToJSONFile(N);
